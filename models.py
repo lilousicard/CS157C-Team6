1f50c050de897fd76ef339def82f3f6acf5f55d5
Jesse2131 <6jessed6@gmail.com>
2023-03-16 15:14:30 -0700

    removed key

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ import os
 
 url = os.environ.get('neo4j+s://41890240.databases.neo4j.io', 'http://localhost:5000')
 
-# graph = Graph(url + '/db/data/', auth=('neo4j', '4CZLYw1ngU7jYPn_LTZi0lUTa7jJoerx2bSb3q8M6lo'))
 graph = Graph('neo4j+s://ea1daa0c.databases.neo4j.io', auth=('neo4j',
                                                              'secret key'))
 matcher = NodeMatcher(graph)
