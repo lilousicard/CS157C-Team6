f6d916e449b3d8250021d47eff081d32a7a7ac36
Spartan <samy1395trh@gmail.com>
2023-03-27 22:27:34 -0700

    create owners node as per register form

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ url = os.environ.get('neo4j+s://41890240.databases.neo4j.io', 'bolt://localhost:
 graph = Graph('neo4j+s://41890240.databases.neo4j.io', auth=('neo4j', 'secret key'))
 matcher = NodeMatcher(graph)
 
-
 class Customer:
     def __init__(self, params):
         self.username = params.get('username')
@@ -15,6 +14,7 @@ class Customer:
         self.age = params.get('age')
         self.gender = params.get('gender')
         self.email = params.get('email')
+        self.owner = params.get('restaurant_owner')
 
     def find(self):
         cust = matcher.match("Customer", self.username).first()
@@ -22,9 +22,15 @@ class Customer:
 
     def register(self):
         if not self.find():
-            cust = Node('Customer', name=self.username, password=self.password, gender=self.gender,
+            if self.owner == None:
+                cust = Node('Customer', name=self.username, password=self.password, gender=self.gender,
                         email=self.email, age=self.age)
-            graph.create(cust)
-            return True
+                graph.create(cust)
+                return True
+            else:
+                owner = Node('Owner', name=self.username, password=self.password, gender=self.gender,
+                            email=self.email, age=self.age)
+                graph.create(owner)
+                return True
         else:
             return False
