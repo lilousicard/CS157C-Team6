54c46cc1e405b03054287fee49ae21bc16c61a81
Jesse Dong <dong@Jesses-MacBook-Air.local>
2023-04-05 23:16:57 -0700

    check issues.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from models import Customer
 
-from flask import Flask, request, render_template, flash, session, redirect, url_for
+from flask import Flask, request, render_template, flash, session,  \
+    redirect, url_for
 import os
 flask_app = Flask(__name__)
 flask_app.secret_key = os.urandom(24)
@@ -10,36 +11,32 @@ flask_app.secret_key = os.urandom(24)
 def index():
     return render_template('home.html')
 
+
 @flask_app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         params = {
-            'username' : username,
-            'password' : password,
-            'gender' : request.form['gender'],
-            'email' : request.form['email'],
-            'age' : request.form['age'],
-            'restaurant_owner' : request.form.get('restaurant')
+            'password': password,
+            'gender': request.form['gender'],
+            'email': request.form['email'],
+            'age': request.form['age'],
+            'restaurant_owner': request.form.get('restaurant')
         }
 
         if len(username) < 1:
             flash('Your username must be at least one character.')
         elif len(password) < 5:
             flash('Your password must be at least 5 characters.')
-        elif not Customer(params).register():
+        elif not Customer(username).register(params):
             flash(' Username already exists.')
         else:
-            session['username'] = username
+            session['user'] = username
             flash('Logged in.')
             return redirect(url_for('index'))
 
-    return render_template('signUp.html')
-
-
-
-#*******************************************
+    return render_template('register.html')
 
 
 @flask_app.route('/restaurant')
@@ -52,10 +49,21 @@ def home():
 def signup():
     return render_template('signup.html')
 
-@flask_app.route('/login')
-def login():
-    return render_template('login.html')
 
+@flask_app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if not Customer(username).verify_password(password):
+            flash('Invalid login.')
+        else:
+            session['user'] = username
+            flash('Logged in.')
+            return redirect(url_for('index'))
+
+    return render_template('login.html')
 
 @flask_app.route('/account')
 def account():
@@ -65,29 +73,38 @@ def account():
 @flask_app.route('/search')
 def search():
     return render_template('search.html')
-    
+
+@flask_app.route('/logout')
+def logout():
+    session.pop('username', None)
+    session.pop('user', None)
+    flash('Logged out.')
+    return redirect(url_for('index'))
+
+
+@flask_app.route('/add_friend', methods=["GET", "POST"])
+def add_friend():
+    username = session['user']
+    Customer(username).add_friend("bob")
+    return redirect(url_for('index'))
+
 @flask_app.route('/review')
 def review():
     return render_template('review.html')
+
 
 @flask_app.route('/editForm')
 def restaurantForm():
     return render_template('restaurantForm.html')
 
+
 @flask_app.route('/otherProfile')
 def otherPeople():
     return render_template('otherProfile.html')
 
-#*******************************************
-
-
-
-
 
 def main():
-    print("Hello World!")
     flask_app.run(port=5001, debug=True)
-
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,35 +2,59 @@ from py2neo import Graph, Node, Relationship, NodeMatcher
 from passlib.hash import bcrypt
 import os
 
-url = os.environ.get('neo4j+s://41890240.databases.neo4j.io', 'bolt://localhost:7687')
+url = os.environ.get("neo4j+s://ea1daa0c.databases.neo4j.io",
+                     "bolt://localhost:7687")
 
-graph = Graph('neo4j+s://41890240.databases.neo4j.io', auth=('neo4j', 'secret key'))
+graph = Graph("neo4j+s://ea1daa0c.databases.neo4j.io",
+              auth=("neo4j", "4CZLYw1ngU7jYPn_LTZi0lUTa7jJoerx2bSb3q8M6lo"))
 matcher = NodeMatcher(graph)
 
+
 class Customer:
-    def __init__(self, params):
-        self.username = params.get('username')
-        self.password = params.get('password')
-        self.age = params.get('age')
-        self.gender = params.get('gender')
-        self.email = params.get('email')
-        self.owner = params.get('restaurant_owner')
+    def __init__(self, username):
+        self.username = username
 
     def find(self):
-        cust = matcher.match("Customer", self.username).first()
+        cust = matcher.match("Customer", username=self.username).first()
         return cust
 
-    def register(self):
+    def verify_password(self, password):
+        cust = self.find()
+        if cust:
+            return password == cust["password"]
+            # return bcrypt.verify(password, cust["password"])
+        else:
+            return False
+
+    def register(self, params):
+        password = params.get("password")
+        age = params.get("age")
+        gender = params.get("gender")
+        email = params.get("email")
+        owner = params.get("restaurant_owner")
+        # encrypt pass later with bcrypt
         if not self.find():
-            if self.owner == None:
-                cust = Node('Customer', name=self.username, password=self.password, gender=self.gender,
-                        email=self.email, age=self.age)
+            if not owner:
+                cust = Node("Customer", username=self.username,
+                            password=password, gender=gender,
+                            email=email, age=age)
                 graph.create(cust)
                 return True
             else:
-                owner = Node('Owner', name=self.username, password=self.password, gender=self.gender,
-                            email=self.email, age=self.age)
+                owner = Node("Owner", name=self.username,
+                             password=password, gender=gender,
+                             email=email, age=age)
                 graph.create(owner)
                 return True
         else:
             return False
+
+    def add_friend(self, friend):
+        cur_user = self.find()
+        # search for the user that has been sent a friend request
+        friend_node = matcher.match("Customer", username=friend).first()
+        # create connection of friend_node was found(Should be no error)
+        if friend_node:
+            graph.create(Relationship(cur_user, "FRIENDS", friend_node))
+        else:
+            print(f"User {friend} doesn't exist")
